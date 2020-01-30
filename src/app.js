95d0df77c82a5e66cc0ae3354119a14f136091c7
GarethBeer <gareth.beer1989@outlook.com>
2020-01-30 19:14:03 +0000

    working on passing tests

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
-const bodyParser = require('body-parser');
+/* const bodyParser = require('body-parser'); */
 
 const artistControllers = require('./Controllers/artists');
 
 const app = express();
+app.use(express.json());
 
 app.post('/artists', artistControllers.create);
 
+app.get('/artists', artistControllers.list);
 module.exports = app;
